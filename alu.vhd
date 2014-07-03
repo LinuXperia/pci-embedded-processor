6b82326807c3ba36b2dab725c52a3e0dc485b989
Nestor Júnior <nestor.tmj@gmail.com>
2014-07-03 19:59:50 -0300

    Correção de erros do alu.vhd

--- alu.vhd
+++ alu.vhd
@@ -1,45 +1,45 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use work.processor_functions.all;
-entity alu is
-    Port (
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE work.processor_functions.all;
+ENTITY alu IS
+    PORT (
         Clk : IN std_logic;
         Nrst : IN std_logic;
-        ALU_cmd : IN std_logic_vector(2 downto 0);
+        ALU_cmd : IN std_logic_vector(2 DOWNTO 0);
         ALU_zero : OUT std_logic;
         ALU_valid : IN std_logic;
-        ALU_bus : INOUT std_logic_vector(n-1 downto 0)
+        ALU_bus : INOUT std_logic_vector(n-1 DOWNTO 0)
     );
-End entity alu;
+END ENTITY alu;
 
-architecture RTL of ALU is
-    signal ACC : std_logic_vector (n-1 downto 0);
-begin
-    ALU_bus <= ACC
-        when ACC_valid  '1' else (others => 'Z');
-    ALU_zero <= '1' when acc  reg_zero else '0';
-    process (clk, nrst) is
-    begin
-        if nrst = '0' then
+ARCHITECTURE RTL OF ALU IS
+    SIGNAL ACC : std_logic_vector (n-1 DOWNTO 0);
+BEGIN
+    ALU_bus <= ACC WHEN ACC_valid = '1' ELSE (others => 'Z');
+    ALU_zero <= '1' WHEN acc = reg_zero ELSE '0';
+    PROCESS (clk, nrst) IS
+    BEGIN
+        IF nrst = '0' THEN
             ACC <= (others => '0');
-        elsif rising_edge(clk) then
-            case ACC_cmd is
-            -- Load the Bus value into the accumulator
-            when "000" => ACC <= ALU_bus;
-            -- Add the ACC to the Bus value
-            When "001" => ACC <= add(ACC,ALU_bus);
-            -- NOT the Bus value
-            When "010" => ACC <= NOT ALU_bus;
-            -- OR the ACC to the Bus value
-            When "011" => ACC <= ACC or ALU_bus;
-            -- AND the ACC to the Bus value
-            When "100" => ACC <= ACC and ALU_bus;
-            -- XOR the ACC to the Bus value
-            When "101" => ACC <= ACC xor ALU_bus;
-            -- Increment ACC
-            When "110" => ACC <= ACC + 1;
-            -- Store the ACC value
-            When "111" => ALU_bus <= ACC;
-        end if;
-    end process;
-end architecture RTL;
+        ELSIF rising_edge(clk) THEN
+            CASE ACC_cmd IS
+					-- Load the Bus value into the accumulator
+					WHEN "000" => ACC <= ALU_bus;
+					-- Add the ACC to the Bus value
+					WHEN "001" => ACC <= add(ACC,ALU_bus);
+					-- NOT the Bus value
+					WHEN "010" => ACC <= NOT ALU_bus;
+					-- OR the ACC to the Bus value
+					WHEN "011" => ACC <= ACC or ALU_bus;
+					-- AND the ACC to the Bus value
+					WHEN "100" => ACC <= ACC and ALU_bus;
+					-- XOR the ACC to the Bus value
+					WHEN "101" => ACC <= ACC xor ALU_bus;
+					-- Increment ACC
+					WHEN "110" => ACC <= ACC + 1;
+					-- Store the ACC value
+					WHEN "111" => ALU_bus <= ACC;
+				END CASE;
+        END IF;
+    END PROCESS;
+END ARCHITECTURE RTL;
