f41c8eee16893e072da42f79d5a3b561e6a33b18
Nestor Júnior <nestor.tmj@gmail.com>
2014-07-03 20:22:45 -0300

    Removendo erros de compilação do Controller e do PC

--- controller.vhd
+++ controller.vhd
@@ -1,49 +1,49 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use work.processor_functions.all;
-entity controller is
-    generic (
-        n : integer := 16
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE work.processor_functions.all;
+ENTITY controller IS
+    GENERIC (
+        n: integer := 16
     );
-    Port (
-        Clk : IN std_logic;
-        Nrst : IN std_logic;
-        IR_load : OUT std_logic;
-        IR_valid : OUT std_logic;
-        IR_address : OUT std_logic;
-        PC_inc : OUT std_logic;
-        PC_load : OUT std_logic;
-        PC_valid : OUT std_logic;
-        MDR_load : OUT std_logic;
-        MAR_load : OUT std_logic;
-        MAR_valid : OUT std_logic;
-        M_en : OUT std_logic;
-        M_rw : OUT std_logic;
-        ALU_cmd : OUT std_logic_vector(2 downto 0);
-        CONTROL_bus : INOUT std_logic_vector(n-1 
-        downto 0)
+    PORT (
+        Clk: IN std_logic;
+        Nrst: IN std_logic;
+        IR_load: OUT std_logic;
+        IR_valid: OUT std_logic;
+        IR_address: OUT std_logic;
+        PC_inc: OUT std_logic;
+        PC_load: OUT std_logic;
+        PC_valid: OUT std_logic;
+        MDR_load: OUT std_logic;
+        MAR_load: OUT std_logic;
+        MAR_valid: OUT std_logic;
+        M_en: OUT std_logic;
+        M_rw: OUT std_logic;
+        ALU_cmd: OUT std_logic_vector(2 DOWNTO 0);
+        CONTROL_bus: INOUT std_logic_vector(n-1 
+        DOWNTO 0)
     );
-End entity controller;
+END ENTITY controller;
 
-architecture RTL of controller is
-    type states is 
+ARCHITECTURE RTL OF controller IS
+    TYPE states IS 
         (s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10);
-    signal current_state, next_state : states;
-begin
-    state_sequence: process (clk, nrst) is
-        if nrst = '0' then
+    SIGNAL current_state, next_state: states;
+BEGIN
+    state_sequence: PROCESS (clk, nrst) BEGIN
+        IF nrst = '0' THEN
             current_state <= s0;
-        else
-            if rising_edge(clk) then
+        ELSE
+            IF rising_edge(clk) THEN
                 current_state <= next_state;
-            end if;
-        end if;
-    end process state_sequence;
+            END IF;
+        END IF;
+    END PROCESS state_sequence;
 
-    state_machine : process ( present_state, opcode ) is
+    state_machine: PROCESS ( present_state, opcode ) IS
 
     begin
-        -- Reset all the control signals
+        -- Reset all the control SIGNALs
         IR_load <= '0';
         IR_valid <= '0';
         IR_address <= '0';
@@ -55,53 +55,53 @@ begin
         MAR_valid <= '0';
         M_en <= '0';
         M_rw <= '0';
-        Case current_state is
-        When s0 =>
+        Case current_state IS
+        WHEN s0 =>
             PC_valid <= '1'; MAR_load <= '1';
             PC_inc <= '1'; PC_load <= '1';
             Next_state <= s1;
-        When s1 =>
+        WHEN s1 =>
             M_en <='1'; M_rw <= '1';
             Next_state <= s2;
-        When s2 =>
+        WHEN s2 =>
             MDR_valid <= '1'; IR_load <= '1';
             Next_state <= s3;
-        When s3 =>
+        WHEN s3 =>
             MAR_load <= '1'; IR_address <= '1';
-            If opcode = STORE then
+            If opcode = STORE THEN
                 Next_state <= s4;
-            else
+            ELSE
                 Next_state <=s6;
-            End if;
-        When s4 =>
+            END IF;
+        WHEN s4 =>
             MDR_load <= '1'; ACC_valid <= '1';
             Next_state <= s5;
-        When s5 =>
+        WHEN s5 =>
             M_en <= '1';
             Next_state <= s0;
-        When s6 =>
+        WHEN s6 =>
             M_en <= '1'; M_rw <= '1';
-            If opcode = LOAD then
+            If opcode = LOAD THEN
                 Next_state <= s7;
-            else
+            ELSE
                 Next_state <= s8;
-            End if;
-        When s7 =>
+            END IF;
+        WHEN s7 =>
             MDR_valid <= '1'; ACC_load <= '1';
             Next_state <= s0;
-        When s8 =>
+        WHEN s8 =>
             M_en<='1'; M_rw <= '1';
-            If opcode = ADD then
+            If opcode = ADD THEN
                 Next_state <= s9;
-            else
+            ELSE
                 Next_state <= s10;
-            End if;
-        When s9 =>
+            END IF;
+        WHEN s9 =>
             ALU_add <= '1';
             Next_state <= s0;
-        When s10 =>
+        WHEN s10 =>
             ALU_sub <= '1';
             Next_state <= s0;
-        End case;
-    End process state_machine;
-end architecture;
+        END CASE;
+    END PROCESS state_machine;
+END ARCHITECTURE;
--- pc.vhd
+++ pc.vhd
@@ -1,34 +1,37 @@
-library ieee;
-use ieee.std_logic_1164.all;
-entity pc is
-    Port (
-        Clk : IN std_logic;
-        Nrst : IN std_logic;
-        PC_inc : IN std_logic;
-        PC_load : IN std_logic;
-        PC_valid : IN std_logic;
-        PC_bus : INOUT std_logic_vector(n-1 downto 0)
+LIBRARY ieee;
+USE ieee.numeric_std.all;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_signed.all;	
+ENTITY pc IS
+    GENERIC (
+        n: INTEGER := 16
     );
-End entity PC;
+    PORT (
+        Clk: IN STD_LOGIC;
+        Nrst: IN STD_LOGIC;
+        PC_inc: IN STD_LOGIC;
+        PC_load: IN STD_LOGIC;
+        PC_valid: IN STD_LOGIC;
+        PC_bus: INOUT STD_LOGIC_VECTOR(n-1  DOWNTO 0)
+    );
+END ENTITY PC;
 
-architecture RTL of PC is
-    signal counter : unsigned (n-1 downto 0);
-begin
-    PC_bus <= std_logic_vector(counter)
-                when PC_valid = '1' else (others => 
-                    'Z');
-    process (clk, nrst) is
-    begin
-        if nrst = '0' then
-            count <= 0;
-        elsif rising_edge(clk) then
-            if PC_inc = '1' then
-                count <= count + 1;
-            else
-                if PC_load = '1' then
-                    count <= unsigned(PC_bus);
-                end if;
-            end if;
-        end if;
-    end process;
-end architecture RTL;
+ARCHITECTURE RTL OF PC IS
+    SIGNAL counter: INTEGER RANGE 0 to 2**n -1;
+BEGIN
+    PC_bus <= STD_LOGIC_VECTOR(to_unsigned(counter, PC_bus'length)) WHEN PC_valid = '1' ELSE (OTHERS => 'Z');
+    PROCESS (clk, nrst) IS
+    BEGIN
+        IF nrst = '0' THEN
+            counter <= 0;
+        ELSIF rising_edge(clk) THEN
+            IF PC_inc = '1' THEN
+                counter <= counter + 1;
+            ELSE
+                IF PC_load = '1' THEN
+                    counter <= to_integer(unsigned(PC_bus));
+                END IF;
+            END IF;
+        END IF;
+    END PROCESS;
+END ARCHITECTURE RTL;
