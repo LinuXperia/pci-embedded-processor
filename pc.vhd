ad0c9d82bc2fdbdd1340676808a18bae8aaf6468
Nestor Júnior <nestor.tmj@gmail.com>
2014-07-06 12:00:32 -0300

    Adição de comentários explicativos em pc.vhd

--- pc.vhd
+++ pc.vhd
@@ -2,36 +2,38 @@ LIBRARY ieee;
 USE ieee.numeric_std.all;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_signed.all;	
+USE work.processor_functions.all;
 ENTITY pc IS
-    GENERIC (
-        n: INTEGER := 16
-    );
-    PORT (
-        Clk: IN STD_LOGIC;
-        Nrst: IN STD_LOGIC;
-        PC_inc: IN STD_LOGIC;
-        PC_load: IN STD_LOGIC;
-        PC_valid: IN STD_LOGIC;
-        PC_bus: INOUT STD_LOGIC_VECTOR(n-1  DOWNTO 0)
-    );
-END ENTITY PC;
+	PORT (clk, nrst: IN STD_LOGIC; -- reset ativo em zero
+				PC_inc: IN STD_LOGIC; -- sinal que indica que o PC deve ser incrementado
+				PC_load: IN STD_LOGIC; -- sinal que indica que PC deve ser substituído pelo valor em PC_bus
+				PC_valid: IN STD_LOGIC; -- sinal que indica que o valor de PC deve ser colocado em PC_bus (ou Z se 0)
+				PC_bus: INOUT STD_LOGIC_VECTOR(n-1  DOWNTO 0)); -- barramento de entrada/saida
+END ENTITY pc;
 
-ARCHITECTURE RTL OF PC IS
-    SIGNAL counter: INTEGER RANGE 0 to 2**n -1;
+ARCHITECTURE rtl OF pc IS
+	SIGNAL counter: INTEGER RANGE 0 to 2**n -1; -- contador em si
 BEGIN
-    PC_bus <= STD_LOGIC_VECTOR(to_unsigned(counter, PC_bus'length)) WHEN PC_valid = '1' ELSE (OTHERS => 'Z');
-    PROCESS (clk, nrst) IS
-    BEGIN
-        IF nrst = '0' THEN
-            counter <= 0;
-        ELSIF rising_edge(clk) THEN
-            IF PC_inc = '1' THEN
-                counter <= counter + 1;
-            ELSE
-                IF PC_load = '1' THEN
-                    counter <= to_integer(unsigned(PC_bus));
-                END IF;
-            END IF;
-        END IF;
-    END PROCESS;
-END ARCHITECTURE RTL;
+	-- Se o PC_valid = '1', manda o valor do PC pro barramento. Caso contrário, manda Z.
+	PC_bus <= STD_LOGIC_VECTOR(to_unsigned(counter, PC_bus'length)) 
+					WHEN PC_valid = '1' 
+						ELSE (OTHERS => 'Z');
+	
+	PROCESS (clk, nrst) IS
+	BEGIN
+	
+		-- De forma assincrona, se o reset ficar em nível 0, volta o contador pra 0
+		IF nrst = '0' THEN
+			counter <= 0;
+		-- Se teve uma borda de subida no clock, faz as outras coisas
+		ELSIF (clk'EVENT AND clk='1') THEN
+			-- A maior prioridade é do incremento. Se esta em 1, incrementa o PC
+			IF PC_inc = '1' THEN
+				counter <= counter + 1;
+			-- Caso contrario, verifica se é pra carregar o valor do bus.
+			ELSIF PC_load = '1' THEN
+					counter <= to_integer(unsigned(PC_bus)); -- Cast de STD_LOGIC_VECTOR pra INTEGER
+			END IF;
+		END IF;
+	END PROCESS;
+END ARCHITECTURE rtl;
