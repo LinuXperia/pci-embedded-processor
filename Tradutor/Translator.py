0117bb34f67144530827932043653668dfa15dfe
Gearlles <gearlles@gmail.com>
2014-07-13 13:06:23 -0300

    Tradutor: conversão da instrução e parametro.

--- Tradutor/Translator.py
+++ Tradutor/Translator.py
@@ -1,4 +1,63 @@
-filename = './input.asm'
-with open(filename) as f:
+import re
+
+input_filename = './input.asm'
+output_filename = './output.txt'
+
+regexes = {
+    'decimal_regex': '^([a-zA-Z]*)(\s(\d*))?$',
+    'hexadecimal_regex': '^([a-zA-Z]*)(\s0x([a-fA-F0-9]*))?$'
+}
+
+# tuple (address, unary)
+encode = {
+    'load': ('0000', False),
+    'store': ('0001', False),
+    'add': ('0010', False),
+    'sub': ('0011', False),
+    'inc': ('0100', True),
+    'dec': ('0101', True),
+    'not': ('0110', True),
+    'and': ('0111', False),
+    'or': ('1000', False),
+    'xor': ('1001', False),
+    'jump': ('1010', False),
+    'be': ('1011', False),
+    'bg': ('1100', False),
+    'bl': ('1101', False),
+    'wait': ('1110', True),
+    'nop': ('1111', True)
+}
+
+with open(input_filename) as f:
     for line in f:
-        print line
+        line = line.split('--')[0]  # ignore comments
+        line = line.strip()  # ignore whitespaces
+
+        numeric_type = ''
+        instruction = ''
+        parameter = ''
+
+        for key in regexes:
+            regex = re.compile(regexes[key])
+            mat = regex.match(line)
+
+            if mat is not None:
+                numeric_type = key
+                break
+
+        if not numeric_type:
+            raise ValueError('Syntax error on line: ', line)
+
+        try:
+            encoded_value = encode[mat.groups()[0].lower()]
+        except KeyError:
+            raise ValueError('Unknown instruction on line: ', line)
+
+        parameter = mat.groups()[1]
+
+        if encoded_value[1] is False and not parameter:
+            raise ValueError('Expected parameter on line: ', line)
+
+        instruction = encoded_value[0]
+
+        print instruction, parameter
--- Tradutor/input.asm
+++ Tradutor/input.asm
@@ -1,2 +1,25 @@
-testea
-testeb
+LOAD 25 -- comentarios funcionam e upper case tambem
+inc
+add 26
+dec
+sub 27
+store 28
+or 25
+xor 26
+and 29
+not
+and 25
+store 30
+sub 25
+bg 17
+be 17
+add 26
+store 31
+load 26
+sub 25
+bl 33
+store 32
+load 28
+add 30
+add 31
+store 0x25
