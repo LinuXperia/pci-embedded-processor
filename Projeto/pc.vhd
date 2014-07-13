44c2f9de57078fec768ca6d7ba377f2a655059a0
Nestor Júnior <nestor.tmj@gmail.com>
2014-07-13 13:56:33 -0300

    Adição de exibição do PC no hex5 e hex5. Ajuste do hexadecimal "A" no bcd_to_7seg para ser maiusculo.

--- Projeto/pc.vhd
+++ Projeto/pc.vhd
@@ -10,16 +10,28 @@ ENTITY pc IS
 				PC_inc: IN STD_LOGIC; -- sinal que indica que o PC deve ser incrementado
 				PC_load: IN STD_LOGIC; -- sinal que indica que PC deve ser substitui­do pelo valor em PC_bus
 				PC_valid: IN STD_LOGIC; -- sinal que indica que o valor de PC deve ser colocado em PC_bus (ou Z se 0)
-				PC_bus: INOUT STD_LOGIC_VECTOR(n-1  DOWNTO 0)); -- barramento de entrada/saida
+				PC_bus: INOUT STD_LOGIC_VECTOR(n-1  DOWNTO 0);
+				PC_7seg: OUT STD_LOGIC_VECTOR(0 TO 15)); -- barramento de entrada/saida
 END ENTITY pc;
 ------------------------------------------------------------------------------------------------------------------
 ARCHITECTURE rtl OF pc IS
 	SIGNAL counter: INTEGER RANGE 0 to 2**n -1; -- contador em si
+	SIGNAL counter_vector: STD_LOGIC_VECTOR(n-1  DOWNTO 0);
+	COMPONENT bcd_to_7seg IS
+		PORT (bcd: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+				output: OUT STD_LOGIC_VECTOR (0 TO 7));
+	END COMPONENT;
 BEGIN
 	-- Se o PC_valid = '1', manda o valor do PC pro barramento. Caso contrario, manda Z.
-	PC_bus <= STD_LOGIC_VECTOR(to_unsigned(counter, PC_bus'length)) 
+	PC_bus <= counter_vector
 					WHEN PC_valid = '1' 
 						ELSE (OTHERS => 'Z');
+						
+	counter_vector <= STD_LOGIC_VECTOR(to_unsigned(counter, PC_bus'length));
+	
+	-- Gera a visualizacao 7seg 
+	counter7seg_0: bcd_to_7seg PORT MAP(counter_vector(3 DOWNTO 0), PC_7seg(0 TO 7));
+	counter7seg_1: bcd_to_7seg PORT MAP(counter_vector(7 DOWNTO 4), PC_7seg(8 TO 15));
 	
 	PROCESS (clk, nrst) IS
 	BEGIN
