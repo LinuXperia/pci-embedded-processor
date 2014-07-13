d3c2f3fb102d212fd2e7a8c92bcaa7aec088bbf5
Nestor Júnior <nestor.tmj@gmail.com>
2014-07-13 09:55:58 -0300

    Removendo entidade "controller" antiga, trocando pela "controller_simplified"

--- Projeto/controller.vhd
+++ Projeto/controller.vhd
@@ -27,14 +27,27 @@ ENTITY controller IS
 			-- ALU
 			ALU_zero: IN std_logic;
 			ALU_valid: OUT std_logic;
+			ALU_slt: IN std_logic;
 			ALU_enable: OUT std_logic;
-			ALU_cmd: OUT std_logic_vector(2 DOWNTO 0));
+			ALU_cmd: OUT std_logic_vector(2 DOWNTO 0);
+			
+			-- IO
+			IODR_load: OUT STD_LOGIC;
+			IOAR_load: OUT STD_LOGIC;
+			IO_valid: OUT STD_LOGIC;
+			IO_en: OUT STD_LOGIC;
+			IO_rw: OUT STD_LOGIC);
+			
 END ENTITY controller;
 ------------------------------------------------------------------------------------------------------------------
 ARCHITECTURE RTL OF controller IS
-	TYPE states IS (s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16);
+	TYPE states IS (s0, s1, s2, s3, s4, s5, s6, s7, s9, s10, s11);
 	SIGNAL current_state, next_state: states;
+	SIGNAL BRANCH_trigger: std_logic;
 BEGIN
+
+	BRANCH_trigger <= '1' WHEN ((IR_opcode = BZERO AND ALU_zero = '1') OR (IR_opcode = BLESS AND ALU_slt = '1') OR (IR_opcode = BGREATER AND ALU_zero = '0' AND ALU_slt = '0')) ELSE '0';
+	
 	-- Processo que gerencia a transicao do current_state para o next_state
 	-- e a configuracao de reset
 	state_sequence: PROCESS (clk, nrst) BEGIN
@@ -65,6 +78,11 @@ BEGIN
 		ALU_valid <= '0';
 		ALU_enable <= '0';
 		ALU_cmd <= "000";
+		IODR_load <= '0';
+		IOAR_load <= '0';
+		IO_valid <= '0';
+		IO_en <= '0';
+		IO_rw <= '0';
 
 		CASE current_state IS
 			WHEN s0 =>
@@ -81,11 +99,15 @@ BEGIN
 				MEM_valid <= '1';
 				IR_load <= '1';
 				IF (IR_opcode = INC) THEN
-					next_state <= s13;
+					next_state <= s7;
 				ELSIF (IR_opcode = JUMP) THEN
-					next_state <= s15;
-				ELSIF (IR_opcode = BZERO) THEN
-					next_state <= s14;
+					next_state <= s10;
+				ELSIF (IR_opcode = BZERO OR IR_opcode = BGREATER OR IR_opcode = BLESS) THEN
+					next_state <= s9;
+				ELSIF (IR_opcode = NOP) THEN
+					next_state <= s0;
+				ELSIF (IR_opcode = WAITT) THEN
+					next_state <= s11;
 				ELSE
 					next_state <= s3;
 				END IF;
@@ -93,6 +115,7 @@ BEGIN
 			WHEN s3 =>
 				IR_valid <= '1';
 				MAR_load <= '1';
+				IOAR_load <= '1';
 				If (IR_opcode = STORE) THEN
 					next_state <= s4;
 				ELSE
@@ -102,88 +125,42 @@ BEGIN
 			WHEN s4 =>
 				ALU_valid <= '1';
 				MDR_load <= '1';
+				IODR_load <= '1';
 				next_state <= s5;
 
 			WHEN s5 =>
 				MEM_en <= '1';
 				MEM_rw <= '1';
+				IO_en <= '1';
+				IO_rw <= '1';
 				next_state <= s0;
 
 			WHEN s6 =>
 				MEM_en <= '1';
-				If (IR_opcode = LOAD) THEN
-					next_state <= s7;
-				ELSIF (IR_opcode = ADD) THEN
-					next_state <= s8;
-				ELSIF (IR_opcode = SUB) THEN
-					next_state <= s16;
-				ELSIF (IR_opcode = NOTT) THEN
-					next_state <= s9;
-				ELSIF (IR_opcode = ORR) THEN
-					next_state <= s10;
-				ELSIF (IR_opcode = ANDD) THEN
-					next_state <= s11;
-				ELSIF (IR_opcode = XORR) THEN
-					next_state <= s12;
-				END IF;
+				IO_en <= '1';
+				next_state <= s7;
 
 			WHEN s7 =>
 				MEM_valid <= '1';
+				IO_valid <= '1';
 				ALU_enable <= '1';
-				ALU_cmd <= "000";
-				next_state <= s0;
-
-			WHEN s8 =>
-				MEM_valid <= '1';
-				ALU_enable <= '1';
-				ALU_cmd <= "001";
+				ALU_cmd <= cmdDecode(IR_opcode);
 				next_state <= s0;
 
 			WHEN s9 =>
-				MEM_valid <= '1';
-				ALU_enable <= '1';
-				ALU_cmd <= "010";
-				next_state <= s0;
-			
-			WHEN s10 =>
-				MEM_valid <= '1';
-				ALU_enable <= '1';
-				ALU_cmd <= "011";
-				next_state <= s0;
-
-			WHEN s11 =>
-				MEM_valid <= '1';
-				ALU_enable <= '1';
-				ALU_cmd <= "100";
-				next_state <= s0;
-
-			WHEN s12 =>
-				MEM_valid <= '1';
-				ALU_enable <= '1';
-				ALU_cmd <= "101";
-				next_state <= s0;
-
-			WHEN s13 =>
-				ALU_enable <= '1';
-				ALU_cmd <= "110";
-				next_state <= s0;
-
-			WHEN s14 =>
-				IF (ALU_zero = '1') THEN
-					next_state <= s15;
+				IF (BRANCH_trigger = '1') THEN
+					next_state <= s10;
 				ELSE
 					next_state <= s0;
 				END IF;
 
-			WHEN s15 =>
+			WHEN s10 =>
 				PC_load <= '1';
 				IR_valid <= '1';
 				next_state <= s0;
-
-			WHEN s16 =>
-				MEM_valid <= '1';
-				ALU_enable <= '1';
-				ALU_cmd <= "111";
+			
+			WHEN s11 =>
+				-- como fazer o wait?
 				next_state <= s0;
 				
 		END CASE;
--- Projeto/processor.vhd
+++ Projeto/processor.vhd
@@ -45,7 +45,7 @@ ARCHITECTURE processor OF processor IS
 	SIGNAL IO_rw: std_logic;
 	
 BEGIN
-	controller : entity work.controller_simplified port map(clk, nrst, CONTROL_bus, IR_opcode, IR_load, IR_valid, PC_inc, PC_load, PC_valid, MDR_load, MAR_load, MEM_valid, MEM_en, MEM_rw, ALU_zero, ALU_valid, ALU_slt, ALU_enable, ALU_cmd, IODR_load, IOAR_load, IO_valid, IO_en, IO_rw);
+	controller : entity work.controller port map(clk, nrst, CONTROL_bus, IR_opcode, IR_load, IR_valid, PC_inc, PC_load, PC_valid, MDR_load, MAR_load, MEM_valid, MEM_en, MEM_rw, ALU_zero, ALU_valid, ALU_slt, ALU_enable, ALU_cmd, IODR_load, IOAR_load, IO_valid, IO_en, IO_rw);
 	memory : entity work.memory port map(clk, nrst, MDR_load, MAR_load, MEM_valid, MEM_en, MEM_rw, CONTROL_bus);
 	alu : entity work.alu port map(clk, nrst, ALU_cmd, ALU_zero, ALU_slt, ALU_valid, ALU_enable, CONTROL_bus);
 	ir : entity work.ir port map(clk, nrst, IR_load, IR_valid, IR_address, IR_opcode, CONTROL_bus);
