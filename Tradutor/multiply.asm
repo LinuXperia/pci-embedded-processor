59c6cfb9e03be0d45a1f6738dcd40e7da2b44272
Gearlles <gearlles@gmail.com>
2014-07-13 21:37:54 -0300

    Melhorias no multiplicador

--- Tradutor/multiply.asm
+++ Tradutor/multiply.asm
@@ -1,18 +1,23 @@
 -- Realiza a operação z * y
-load 15 -- carrega y
-store 13 -- x = y  escreve y no contador (pra decrementar)
-load 13 -- contador
-be 13 -- se contador for 0, exit
-load 16 -- senao, vamo fazer a soma
-add 14 -- faz z + k
-store 16 -- armazena k
-load 13 -- decrementa o contador
+load 17 -- carrega 0
+store 20 -- reseta contador (x)
+store 21 -- reseta resultado (k)
+load 19 -- carrega y
+store 20 -- x = y  escreve y no contador (pra decrementar)
+load 20 -- carrega contador  (INICIO)
+be 14 -- se contador for 0, exit
+load 21 -- senao, vamo fazer a soma
+add 18 -- faz z + k
+store 22 -- armazena k
+load 20 -- decrementa o contador
 dec
-store 13
-jump 2
-jump 14
-jump 13
-0 -- x (contador)
+store 20
+jump 5 -- (FIM)
+load 21 -- carrega resultado
+store 129 -- print
+jump 0 -- reinicia código
+0 -- sempre é zero
 5 -- z (entrada)
 3 -- y (entrada)
+0 -- x (contador)
 0 -- k (resultado)
