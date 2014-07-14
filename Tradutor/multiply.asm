1f29c1ec5bbd3420c16376ac385870f2494d8c74
Gearlles <gearlles@gmail.com>
2014-07-13 22:10:47 -0300

    Bug fix no código do multiplicador;

--- Tradutor/multiply.asm
+++ Tradutor/multiply.asm
@@ -5,14 +5,14 @@ store 21 -- reseta resultado (k)
 load 19 -- carrega y
 store 20 -- x = y  escreve y no contador (pra decrementar)
 load 20 -- carrega contador  (INICIO)
-be 14 -- se contador for 0, exit
+be 15 -- se contador for 0, exit
 load 21 -- senao, vamo fazer a soma
 add 18 -- faz z + k
 store 22 -- armazena k
 load 20 -- decrementa o contador
 dec
 store 20
-jump 5 -- (FIM)
+jump 6 -- (FIM)
 load 21 -- carrega resultado
 store 129 -- print
 jump 0 -- reinicia código
