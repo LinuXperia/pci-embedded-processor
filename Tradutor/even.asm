fcb0114ce5aca2c887a7c88818f9cf4eb042982e
Gearlles <gearlles@gmail.com>
2014-07-13 22:21:07 -0300

    Bug fix: Esqueci salvar o contador dps de decrementar.

--- Tradutor/even.asm
+++ Tradutor/even.asm
@@ -1,19 +1,20 @@
 -- Imprime o n-ésimo numero par
-load 18 -- carrega 0
-store 20 -- reseta contador
-store 21 -- reseta resultado
-load 17 -- carrega n
+load 19 -- carrega 0
+store 21 -- reseta contador
+store 22 -- reseta resultado
+load 18 -- carrega n
 dec
-store 20 -- contador = n - 1  (contador é iniciado com n - 1 pois o primeiro par é quando nosso contador = 0)
-load 20 -- (INICIO)
-be 14 -- acaba quando contador chega em 0
-load 21 -- carrega resultado
-add 19 --  resultado += 2
-store 21 -- salva resultado
-load 20 -- carrega contador
+store 21 -- contador = n - 1  (contador é iniciado com n - 1 pois o primeiro par é quando nosso contador = 0)
+load 21 -- carrega contador (INICIO)
+be 15 -- acaba quando contador chega em 0
+load 22 -- carrega resultado
+add 20 --  resultado += 2
+store 22 -- salva resultado
+load 21 -- carrega contador
 dec
+store 21
 jump 3 -- itera
-load 21 -- (EXIT) carrega resultado
+load 22 -- (EXIT) carrega resultado
 store 129 -- print
 jump 0 -- reinicia
 3 -- n
